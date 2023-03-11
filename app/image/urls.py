1b2c6990194e679356e6d33d2e1fa71215979a5a
PatrykKerlin <patryk.kerlin@gmail.com>
2023-03-11 08:59:53 +0100

    Changed endpoints names to match REST convention

--- app/image/urls.py
+++ app/image/urls.py
@@ -7,8 +7,8 @@ from rest_framework import routers
 from .views import ImageViewSet, ThumbnailViewSet, LinkViewSet
 
 router = routers.DefaultRouter()
-router.register("image", ImageViewSet)
-router.register("thumbnail", ThumbnailViewSet)
+router.register("images", ImageViewSet)
+router.register("thumbnails", ThumbnailViewSet)
 router.register("link", LinkViewSet, basename="link")
 
 app_name = "image"
